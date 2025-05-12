ad705485dd22bc6a2466c7cb37803a04458a3fc7
Sanskar Shukla <sanskar0627@gmail.com>
2025-05-12 23:38:05 +0530

    testing

---
+++ Week 02 - Async JS/Day-2 Async-JS/recatngle.js
@@ -0,0 +1,18 @@
+class Rectangle{
+    constructor(width,height,colour){
+        this.width=width;
+        this.height=height;
+        this.colour=colour
+    }
+    area(){
+        const area=this.width*this.height;
+        return area;
+    }
+    paint(){
+        console.log("The colour of the recatangle is..."+this.colour);
+    }
+}
+const rect1=new Rectangle(5,8,"Red");
+console.log(rect1.area());
+rect1.paint();
+ 
