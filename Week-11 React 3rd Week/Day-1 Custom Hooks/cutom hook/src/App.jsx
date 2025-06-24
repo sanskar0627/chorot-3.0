9b50acafb6eb669b117698c16e572f826a0b92cc
Sanskar Shukla <sanskar0627@gmail.com>
2025-06-24 17:07:17 +0530

    BAics incease and decase website

--- Week-11 React 3rd Week/Day-1 Custom Hooks/cutom hook/src/App.jsx
+++ Week-11 React 3rd Week/Day-1 Custom Hooks/cutom hook/src/App.jsx
@@ -3,19 +3,17 @@ import './App.css'
 import { usePrev } from './hooks/usePrev';
 
 function App() {
-  const [state,setState]=useState(0);
-  const prev=usePrev(state);
-  
+  const [state, setState] = useState(0);
+  const prev = usePrev(state);
+
   return (
     <>
-    <p>{state}</p>
-   <button onClick={()=>{ setState((curr)=>curr+1); }}>
-  Click Me
-</button>
-
-   <p>The Previous value Was {prev}</p>
+      <p>{state}</p>
+      <button onClick={() => { setState((curr) => curr + 1); }}>
+        Click Me
+      </button>
+      <p>The Previous value Was {prev}</p>
     </>
-
   )
 }
 
---
+++ Week-11 React 3rd Week/Day-2 Recoil (State Management)/Recoil/src/App.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Counter/>
+    </>
+  )
+}
+
+function Increase({setCount}){
+  function increase(){
+    setCount(c=>c+1);
+  }
+  return<div>
+    <button onClick={increase}>
+      Increase
+    </button>
+  </div>
+}
+
+function Decrease({setCount}){
+  function decrease(){
+    setCount(c=>c-1);
+  }
+  return<div>
+    <button onClick={decrease}>
+      decrease
+    </button>
+  </div>
+}
+
+function Counter(){
+  const [count,setCount]=useState(0);
+  return<div>
+    {count}
+    <Increase setCount={setCount}/>
+    <Decrease setCount={setCount}/>
+  </div>
+}
+
+export default App
