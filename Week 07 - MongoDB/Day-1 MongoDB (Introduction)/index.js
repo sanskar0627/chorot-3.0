75f29e9d3816e10187d19194f2b87499c2ade5a7
Sanskar Shukla <sanskar0627@gmail.com>
2025-06-25 12:58:19 +0530

    week 7 Assigments

--- Week 07 - MongoDB/Day-1 MongoDB (Introduction)/index.js
+++ Week 07 - MongoDB/Day-1 MongoDB (Introduction)/index.js
@@ -8,71 +8,95 @@ const JWT_SECRET = "sanskar12345"
 mongoose.connect("mongodb+srv://sanskar0627:9635771173%40Hp@cluster0.ntuj2ya.mongodb.net/Todo-Application");
 
 app.post("/signup", async function (req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
-    res.json({
-        message: "You are Logged In"
-    });
+        await UserModel.create({
+            email: email,
+            password: password,
+            name: name
+        });
+        res.json({
+            message: "You are Logged In"
+        });
+    }
+    catch (err) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
 app.post("/signin", async function (req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+    
+        try {
+            const { email, password } = req.body;
 
-    const user = await UserModel.findOne({
-        email: email,
-        password: password
+            const user = await UserModel.findOne({
+                email: email,
+                password: password
+            });
+            if (user) {
+                const token = jwt.sign({
+                    id: user._id.toString()
+                }, JWT_SECRET);
+                res.json({ message: "Signed in successfully", token });
+            } else {
+                res.status(403).json({
+                    message: "Incorrect credinatials"
+                })
+            }
+        }
+        catch (err) {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
     });
-    if (user) {
-        const token = jwt.sign({
-            id: user._id.toString()
-        }, JWT_SECRET);
-        res.json({ message: "Signed in successfully", token });
-    } else {
-        res.status(403).json({
-            message: "Incorrect credinatials"
+app.post("/todo", auth, async function (req, res) {
+    try {
+        const userId = req.userId;
+        const title = req.body.title;
+        await TodoModel.create({
+            title, userId
+        })
+        res.json({
+            userId: userId
         })
     }
-});
-app.post("/todo", auth, async function (req, res) {
-    const userId = req.userId;
-    const title = req.body.title;
-    await TodoModel.create({
-        title, userId
-    })
-    res.json({
-        userId: userId
-    })
+    catch (err) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 
 });
 app.get("/todos", auth, async function (req, res) {
-    const userId = req.userId;
-    const todos = await TodoModel.find({
-        userId: userId
-    })
-    res.json({
-        todos
-    })
+    try {
+        const userId = req.userId;
+        const todos = await TodoModel.find({
+            userId: userId
+        })
+        res.json({
+            todos
+        })
+    }
+    catch (err) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 function auth(req, res, next) {
-    const token = req.headers.token;
-    const decodedData = jwt.verify(token, JWT_SECRET);
-    if (decodedData) {
-        req.userId = decodedData.id;
-        next();
-    }
-    else {
-        res.status(403).json({
-            message: "Incomplete credentials"
-        });
+    try {
+        const token = req.headers.token;
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        if (decodedData) {
+            req.userId = decodedData.id;
+            next();
+        }
+        else {
+            res.status(403).json({
+                message: "Incomplete credentials"
+            });
 
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 app.listen(3000);
