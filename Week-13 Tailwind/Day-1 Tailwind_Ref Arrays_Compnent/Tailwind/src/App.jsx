25dfa5136ded413025a85ecaea1a92e3c7f740a1
Sanskar Shukla <sanskar0627@gmail.com>
2025-06-27 21:46:14 +0530

    tailwind learning

---
+++ Week-13 Tailwind/Day-1 Tailwind_Ref Arrays_Compnent/Tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className='bg-blue-500'>
+      Sanskar
+     </div>
+    </>
+  )
+}
+
+export default App
