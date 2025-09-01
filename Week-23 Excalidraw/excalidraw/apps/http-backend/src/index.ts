b672fedf27f2602f8aacab881aaed16a1cc23e1a
Sanskar Shukla <sanskar0627@gmail.com>
2025-09-01 11:19:07 +0530

    xyz

--- Week-23 Excalidraw/excalidraw/apps/http-backend/src/index.ts
+++ Week-23 Excalidraw/excalidraw/apps/http-backend/src/index.ts
@@ -1,5 +1,20 @@
-import  express  from "express";
+import express from "express";
+import { JsonWebTokenError } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
-const app=express();
+const app = express();
+
+app.post("/signup", (req, res) => {
+  
+});
+
+app.post("/signin", (req, res) => {
+  
+});
+
+app.post("/room", middleware, (req, res) => {
+
+});
 
 app.listen(3001);
